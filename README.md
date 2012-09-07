789b40125d6c6d3465d9d60dddbb8bf6c3db02c8
Carlos Peñas <carlos.penas@the-cocktail.com>
2012-09-07 09:26:58 +0200

    Prima riesgo

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 munin-primariesgo
 =================
+
+Script to parse http://www.laprimaderiesgo.com And obtain his data formatted as a munin punin plugin
+
+Requires perl and LWP::Simple
+
---
+++ primariesgo.pl
@@ -0,0 +1,42 @@
+#/usr/bin/perl -w
+
+use strict;
+use LWP::Simple;
+
+use LWP::Simple;
+
+my $command = shift;
+
+if ($command eq 'config') {
+  print <<EOF;
+graph_title Prima de Riesgo
+graph_vlabel prima
+graph_category Economy
+francia.label Francia
+italia.label Italia
+espana.label España
+grecia.label Grecia
+irlanda.label Irlanda
+portugal.label Portugal
+EOF
+exit(0);
+}
+
+my @content = split(/\n/,get 'http://www.laprimaderiesgo.com/');
+exit(1) unless @content;
+
+for (@content) {
+  if (/^.h2.([0-9]+)/i){
+    print "espana.value $1\n";
+  }
+}
+
+my @stuff = grep { /14px/ } grep { !/%/ } @content;
+
+for (@stuff){
+  s/\.//;
+  s/^.*\s([a-zA-Z]+):\s+([0-9\.]+).*$/$1.value $2/;
+  print lc;
+  print "\n";
+}
+
